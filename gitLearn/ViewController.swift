d4cd26d6b00dc615c4396df3b823c76f21520e82
Rajesh S <email@address.com>
2020-12-09 14:16:20 +0530

    Initial commit

---
+++ gitLearn/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  gitLearn
+//
+//  Created by Ashish Pandey on 09/12/20.
+//  Copyright © 2020 Simmy Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Ashish Pandey")
+    }
+
+
+}
+
